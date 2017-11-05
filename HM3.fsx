9be12156572b4d0327d73e933f245298c392885e
Rodolfo Marrero <rmarr028@fiu.edu>
2017-11-04 22:26:12 -0400

    Polishing #5

--- HM3.fsx
+++ HM3.fsx
@@ -24,13 +24,20 @@ let rec transpose = function
     | [] -> []
     | list -> (List.map (List.head) list) :: if (List.length (List.head list)) > 1 then (transpose (List.map (remove_first) list)) else [] //Just need to remove first element of each list on this recursive call
 
-//Removes the first element from any list
+(**
+ * Helper for 2 - Helper for transpose
+ * Removes the first element from any list
+ *
+ * @param list - The list that needs the first element removed
+ * @return - The tail of the list
+ *)
 let remove_first = function
     | [] -> []
     | x::xs -> xs
 
 (**
  * 2-
+ * Weird matrix multiplication.
  * Problem: Given an m-by-n matrix A and an n-by-p matrix B, the product of A and B is an 
  * m-by-p matrix whose entry in position (i,j) is the inner product of row i of A with column j of B. For example,
  *                / 0 1 \
@@ -53,13 +60,20 @@ let multiply (matrix1, matrix2) =
 
   inner_multiply (matrix1, matrix2_transposed)
 
-
-//
-multiply ( [ [1;2;3]; [4;5;6] ], [ [0;1]; [3;2]; [1;2] ] );;
-
-//5 - Show how to define map f s on streams; this should give the stream formed by applying function f to each element of stream s.
+(**
+ * 5-
+ * List.map on infinite streams
+ * Problem: Show how to define map f s on streams; this should give the stream formed by applying function f to each element of stream s.
+ * Status: Done
+ *
+ * @param f - a function to apply on the stream
+ * @param Cons(x, xsf) - The stream to apply the function on
+ * @return Cons(y, ysf) - The stream Cons(x, xsf) with the function f applied to it
+ *)
 type 'a stream = Cons of 'a * (unit -> 'a stream)
 
-//This should work
+
 let rec map f (Cons(x, xsf)) = 
     Cons(f x, fun() -> map f (xsf()))
+
+
