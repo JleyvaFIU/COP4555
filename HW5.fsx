f0d9851bc8e4dc80d5ff3a640976fb6414c2bb26
Rodolfo Marrero <rmarr028@fiu.edu>
2017-11-18 17:39:33 -0500

    Adding tons of comments for everyone to understand what's happening. IF function is now being handled successfully.

--- HW5.fsx
+++ HW5.fsx
@@ -1,19 +1,23 @@
 #load "parser.fsx"
 
+//Import the parser
 open Parser.Parse
 
+//Sample use of the parser
 let input = parsestr "iszero (succ 7)";;
 
+//Implementing the interpreter
 let rec interp = function
-    | ERROR x -> ERROR x
-    | NUM n when n > 0 -> NUM n
-    | BOOL b -> BOOL b
-    | SUCC -> SUCC
-    | PRED -> PRED
-    | ISZERO -> ISZERO
-    | IF (BOOL b, e1, e2) -> if b then e1 else e2
-    | APP (SUCC, NUM f2) -> NUM (f2 + 1)
-    | APP (PRED, NUM f2) -> NUM (f2 - 1)
-    | APP (ISZERO, NUM f2) -> BOOL (f2 = 0)
-    | APP (f1, f2) -> interp (APP (f1, interp f2))
+    | ERROR x -> ERROR x //If there is an error, return the error
+    | NUM n when n > 0 -> NUM n //If it is a number (greater than 0, as specified), return the number
+    | BOOL b -> BOOL b //If it is a boolean, return the boolean
+    | SUCC -> SUCC //If it is the SUCC term, return it. We don't evaluate here
+    | PRED -> PRED //If it is the PRED term, return it. We don't evaluate here
+    | ISZERO -> ISZERO //If it is the ISZERO term, return it. We don't evaluate here
+    | IF (BOOL b, e1, e2) -> if b then interp e1 else interp e2 //Evaluate the IF function. b must be a BOOL term, otherwise, fall down
+    | IF (APP (f1, f2), e1, e2) -> interp (IF((interp (APP(f1, f2))), e1, e2)) //Evaluate the IF function. b can be any APP term
+    | APP (SUCC, NUM f2) -> NUM (f2 + 1) //Evaluate the SUCC term
+    | APP (PRED, NUM f2) -> NUM (f2 - 1) //Evaluate the PRED term
+    | APP (ISZERO, NUM f2) -> BOOL (f2 = 0) //Evaluate the ISZERO term
+    | APP (f1, f2) -> interp (APP (f1, interp f2)) //Evaluate any APP term where the second term is not a NUM
     | _ -> failwith "Not valid or not implemented"
